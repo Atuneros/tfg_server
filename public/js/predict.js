98e71b7ce8cf7e7018dcab87d31ca7a6ec83f921
Atunero <jovapal.spam92@gmail.com>
2022-03-30 20:07:00 +0200

    wip

--- public/js/predict.js
+++ public/js/predict.js
@@ -1,17 +1,16 @@
 export async function predict(){
   const model = await tf.loadLayersModel(`http://localhost:30200/api/models/classify`)
   const img = document.getElementById("img")
-  const example = tf.expandDims(tf.browser.fromPixels(img).resizeBilinear([224,224]),0)
-  let prediction = model.predict(example)
-  prediction = prediction.squeeze()
-  // lo del round funciona porque solo hay 2 clases, si hay mas .argmax o explota
-  prediction = prediction.round()
-
-  const canvas = document.getElementById('destination')
-  // canvas.width = prediction.shape.width
-  // canvas.height = prediction.shape.height
-  await tf.browser.toPixels(prediction, canvas)
-  // document.body.appendChild(canvas)
+  if(img.width > 1 && img.height > 1){
+    const example = tf.expandDims(tf.browser.fromPixels(img).resizeBilinear([224,224]),0)
+    let prediction = model.predict(example)
+    prediction = prediction.squeeze()
+    // lo del round funciona porque solo hay 2 clases, si hay mas .argmax o explota
+    prediction = prediction.round()
   
-  console.log(prediction)
+    const canvas = document.getElementById('destination')
+    // canvas.width = prediction.shape.width
+    // canvas.height = prediction.shape.height
+    await tf.browser.toPixels(prediction, canvas)
+  }
 }
